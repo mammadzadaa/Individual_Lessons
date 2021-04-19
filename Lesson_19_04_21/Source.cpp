8ef0ea208ad950bf03175fcf0f68bd29f82f3dc4
mammadzadaa <mammedzadea@itstep.edu.az>
2021-04-19 12:34:19 +0400

    Lesson_19_04_21

---
+++ Lesson_19_04_21/Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	// std::cout << "Hello" << std::endl;
+	/*cout << "Hey";
+	cout << " there!" << endl;*/
+
+	cout << "\tHey there\n" << endl;
+	cout << "How are you! ";
+		// \ - escape sequance
+	cout << " \n ";				// \n new line
+	cout << "\tHello\n";		// \t tab
+	cout << "Hello\a World!\n";	// \a alert sound
+	cout << "Hello\b\n";		// \b backspace
+	cout << "Hello\rAlo\n";		// \r carriage return
+	cout << "\"Hi\"\n";			// \" double quote
+	cout << "C\\MyDoc\n";		// \\ is \
+}
