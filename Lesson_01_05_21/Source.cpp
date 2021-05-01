3948424c7ddf0c41b953f094b8a505856a143377
mammadzadaa <mammedzadea@itstep.edu.az>
2021-05-01 12:33:34 +0400

    lesson_01_05_21

---
+++ Lesson_01_05_21/Source.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Before\n";
+
+	if (true)
+	{
+		cout << "If1\n";
+	}
+	else if (true)
+	{
+		cout << "If2\n";
+	}
+
+	cout << "After" << endl;
+
+
+	//cout << "Before\n";
+
+	//if (true)
+	//{
+	//	cout << "If1\n";
+	//}
+	//if (false)
+	//{
+	//	cout << "If2\n";
+	//}
+
+	//cout << "After" << endl;
+
+	/*if (false)
+	{
+		cout << "If" << endl;
+	}
+	else
+	{
+		cout << "Else" << endl;
+	}
+	
+
+	cout << "After If Else\n";*/
+
+	/*if (1 || 4 == 8)
+	{
+		cout << "True\n";
+	}*/
+
+	/*bool b = true;
+
+	if (b)
+	{
+		
+	}*/
+
+	/*cout << "Before If" << endl;
+
+	if (false)
+		cout << "If" << endl;
+
+	cout << "Hey\n";
+
+	if (true)
+	{
+		cout << "If" << endl;
+	}
+	
+	cout << "After If" << endl;*/
+
+	//const float Pi = 3.14;
+
+	//short a = 123;
+	//int b = a;
+	//b = 65432;
+	//cout << b << endl;
+	//short c = (short)b;
+	//cout << c << endl;
+
+	//double d = b;
+	//d = 3.456;
+	//int f = d;
+	//cout << d << endl;
+	//cout << f << endl;
+
+
+	/*int a = 5;
+	cout << a++ << endl;
+	cout << ++a << endl;*/
+
+	//cout << a - 3 << endl; // 2
+	//cout << a << endl; // 5
+	//cout << (a -= 3) << endl; // 2
+	//cout << a << endl; // 2
+
+	return 0;
+}
