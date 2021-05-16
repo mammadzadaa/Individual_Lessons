f0749829473eaa2c5100d95c378545b7421eb34f
mammadzadaa <mammedzadea@itstep.edu.az>
2021-05-16 14:31:33 +0400

    lesson_07_05_21

---
+++ Lesson_07_05_21/Source.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	for (cout << "For started\n";true;)
+	{
+
+	}
+	for (;;)
+	{
+		cout << "infinity loop\n";
+	}
+
+
+	for 
+		(size_t i = 0; 
+		i < 5; 
+		i++)
+	{
+		cout << i << endl;
+		//++i;
+	}
+
+	//do
+	//{
+	//	cout << "LOOP\n";
+	//} 
+	//while (false);
+
+	//int i = 0;
+	//while 
+	//(true)
+	//{
+	//	cout << "LOOP\n";
+	//	if (++i < 5)
+	//	{
+	//		continue;
+	//	}
+	//	break;
+	//}
+
+
+	//int i = 0;
+	//while 
+	//(true)
+	//{
+	//	cout << "LOOP\n";
+	//	if (++i == 5)
+	//	{
+	//		break;
+	//	}
+	//}
+
+	return 0;
+}
